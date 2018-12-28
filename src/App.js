4a745601cf5d9dc52431b110f37b0f62d566fa4b
Radoslaw Wojdowski <Radoslaw.Wojdowski@pl.ey.com>
2018-12-28 23:41:54 +0100

    heja

--- src/App.js
+++ src/App.js
@@ -6,21 +6,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        I was changed on the Feature branch
+      </p>
+    </div>
     );
   }
 }
